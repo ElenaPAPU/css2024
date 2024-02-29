cf65c6ebbf28096c682ccc8c504c82aa8eee5136
Elena <demo@example.com>
2024-02-29 20:50:11 +0200

    curs6

---
+++ curs6/js/main.js
@@ -0,0 +1,14 @@
+import {Book, citeste, inchisCartea} from './book.js';
+
+
+//  import * from " demo.js"
+//  import { func1, obj2 } from "demo.js"
+
+function citeste(){
+    return "Nu mai citesc!";
+}
+let carte = new Book('Poezii','Mihai Eminescu', 250);
+console.log(carte);
+ console.log(carte.toString());
+ console.log(citeste());
+ console.log(inchidCartea());
